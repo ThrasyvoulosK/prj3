15448303fa00e9bff89d759e035ce092d2d66c8a
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2022-02-03 19:12:23 +0200

    Delete Project3fin directory

--- Project3fin/include/silhouette.h
+++
@@ -1,9 +0,0 @@
-#ifndef SILHOUETTE_H
-#define SILHOUETTE_H
-#include "../include/libraries.h"
-#include "../include/clustering.h"
-#include "../include/preprocessing.h"
-
-list<clustering> centers_for_silhoutte_v(list<vecter> dat,list<vecter> centers);
-
-#endif
